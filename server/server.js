9bd32873c3ceac0daf159cf1dc26da38cb8048ad
Ben Hood <benhood97@btinternet.com>
2024-04-22 17:18:40 -0700

    set cookie options

--- server/server.js
+++ server/server.js
@@ -4,5 +4,15 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 // middleware
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+// cookie set-up
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+
+  // Cookie Options (cookie is kept for 24 hours)
+  maxAge: 24 * 60 * 60 * 1000
+}));
